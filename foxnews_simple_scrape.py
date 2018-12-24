7bc3ba43f140b8ff7140a6d6f1bc9ee1f17951e4
I <ibelyalov@yandex.ru>
2018-12-25 00:13:04 +0300

    Extract category query param

--- foxnews_simple_scrape.py
+++ foxnews_simple_scrape.py
@@ -3,10 +3,11 @@ import urllib
 import requests
 from tqdm import tqdm
 
-URL = 'https://www.foxnews.com/api/article-search?isCategory=true&isTag=false&isKeyword=false&isFixed=false&isFeedUrl=false&searchSelected=politics&contentTypes=%7B"interactive":false,"slideshow":false,"video":false,"article":true%7D'
-d = {
+URL = 'https://www.foxnews.com/api/article-search?isCategory=true&isTag=false&isKeyword=false&isFixed=false&isFeedUrl=false&contentTypes=%7B"interactive":false,"slideshow":false,"video":false,"article":true%7D'
+query = {
     'size': 30, 
-    'offset': 0
+    'offset': 0,
+    'searchSelected': 'politics'
 }
 
 MAX_SIZE = 30
@@ -14,8 +15,8 @@ MAX_OFFSET = 10000
 
 res = []
 for offset in tqdm(range(0, MAX_OFFSET, MAX_SIZE)):
-    d['offset'] = offset
-    url = '{}&{}'.format(URL, urllib.parse.urlencode(d))
+    query['offset'] = offset
+    url = '{}&{}'.format(URL, urllib.parse.urlencode(query))
     response = requests.get(url)
     res.extend(response.json())
     
